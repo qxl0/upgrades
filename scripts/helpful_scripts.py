25a43d587d12ffb3199b4739aa4fd6e4975531b9
Qiang <qxl0@yahoo.com>
2022-02-25 12:49:37 -0500

    Upgrade to BoxV2 from Proxy

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -38,34 +38,34 @@ def encode_function_data(initializer=None, *args):
     return initializer.encode_input(*args)
 
 
-# def upgrade(
-#     account,
-#     proxy,
-#     newimplementation_address,
-#     proxy_admin_contract=None,
-#     initializer=None,
-#     *args
-# ):
-#     transaction = None
-#     if proxy_admin_contract:
-#         if initializer:
-#             encoded_function_call = encode_function_data(initializer, *args)
-#             transaction = proxy_admin_contract.upgradeAndCall(
-#                 proxy.address,
-#                 newimplementation_address,
-#                 encoded_function_call,
-#                 {"from": account},
-#             )
-#         else:
-#             transaction = proxy_admin_contract.upgrade(
-#                 proxy.address, newimplementation_address, {"from": account}
-#             )
-#     else:
-#         if initializer:
-#             encoded_function_call = encode_function_data(initializer, *args)
-#             transaction = proxy.upgradeToAndCall(
-#                 newimplementation_address, encoded_function_call, {"from": account}
-#             )
-#         else:
-#             transaction = proxy.upgradeTo(newimplementation_address, {"from": account})
-#     return transaction
+def upgrade(
+    account,
+    proxy,
+    newimplementation_address,
+    proxy_admin_contract=None,
+    initializer=None,
+    *args
+):
+    transaction = None
+    if proxy_admin_contract:
+        if initializer:
+            encoded_function_call = encode_function_data(initializer, *args)
+            transaction = proxy_admin_contract.upgradeAndCall(
+                proxy.address,
+                newimplementation_address,
+                encoded_function_call,
+                {"from": account},
+            )
+        else:
+            transaction = proxy_admin_contract.upgrade(
+                proxy.address, newimplementation_address, {"from": account}
+            )
+    else:
+        if initializer:
+            encoded_function_call = encode_function_data(initializer, *args)
+            transaction = proxy.upgradeToAndCall(
+                newimplementation_address, encoded_function_call, {"from": account}
+            )
+        else:
+            transaction = proxy.upgradeTo(newimplementation_address, {"from": account})
+    return transaction
